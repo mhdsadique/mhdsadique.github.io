1dfc983ce4e768451f9e8148c87df1d1a8a22346
Muhammed sadique <mhdsadique8@gmail.com>
2023-03-01 11:24:29 +0530

    clothiyard

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -11,6 +11,92 @@ export const Project = () => {
       <Heading  textAlign='center' marginBottom={'50px'}>Projects</Heading>
       <SimpleGrid columns={{lg:1,md:1,sm:1}} gap='50px'>
 {/* ------------main */} {/* /------------box1 */}
+<Box   bgGradient='linear(to-r, red.500, yellow.500)'  boxShadow='dark-lg' p='6' rounded='md' color={'rgba(0,0,0,.9)'}>
+       <Grid gap={'20px'}>
+      <Box>
+        <img src={"https://camo.githubusercontent.com/b59381c77c8b9f47e89632b2a689b1d201df5b6a1b1d5753d00d8d532dfc07f2/68747470733a2f2f692e706f7374696d672e63632f4276395756686a342f53637265656e73686f742d313033342e706e67"} alt="clothiyard" srcset="" />
+      </Box>
+
+        <Heading color={'white'} marginTop={'10px'} size={'md'}>Clotheyard </Heading>
+        <Text fontSize={'2xl'}>Clotheyard, is an shopping mall–based retailer of casual apparel and accessories, principally targeting young adults and teenagers through its Aéropostale stores.</Text>
+        
+        <Text ><Text as='b'>Tech Stack :</Text> React.js | Chackra UI | HTML | CSS | Javascript | Express | Mongoose | Mongodb</Text>
+        <Spacer/>
+        <Box w={'80%'} display='flex' justifyContent='space-around'>
+      
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/PriyanshuPatil/Clotheyard.com">  <Button
+      w='130px'
+  p={4}
+  color='white'
+  fontWeight='bold'
+  borderRadius='md'
+   bgGradient='linear(to-l, #7928CA, #FF0080)'
+  _hover={{
+    bgGradient: 'linear(to-r, red.500, yellow.500)',
+  }}>Source Code
+  <Avatar size='xs' src='https://www.bing.com/th?id=OSK.b15146a8bebfb993c6dad4725c884d6d&w=148&h=148&c=7&o=6&dpr=1.3&pid=SANGAM' alt='github'/>
+  </Button>
+  </a>
+<a target="_blank" rel="noopener noreferrer" href="https://clothiyard.netlify.app/">
+<Button
+      w='100px'
+  p={4}
+  color='white'
+  fontWeight='bold'
+  borderRadius='md'
+   bgGradient='linear(to-l, #7928CA, #FF0080)'
+  _hover={{
+    bgGradient: 'linear(to-r, red.500, yellow.500)',
+  }}>Live <ArrowRightIcon/>
+  </Button>
+  </a>
+  </Box>
+  </Grid>
+      </Box>
+           {/* ---------box4 */}
+           <Box bgGradient= 'linear(to-r, red.500, yellow.500)'  boxShadow='dark-lg' p='6' rounded='md'  color={'rgba(0,0,0,.9)'}>
+        <Grid gap={'20px'}>
+      <Box>
+        <img src={nyka} alt="mailtrap" srcset="" />
+      </Box>
+
+        <Heading color={'white'} marginTop={'10px'} size={'md'}>Nyka </Heading>
+        <Text fontSize={'2xl'}>Nyka is an e-commerce website where all types of Skincare, Eyecare, Hairstyle and many more related products are available for men, women and kids.</Text>
+        
+        <Text><Text as='b'>Tech Stack :</Text> Next.js | Redux | Chackra UI | CSS | Javascript</Text>
+        <Spacer/>
+        <Box w={'80%'} display='flex' justifyContent='space-around'>
+      
+        <a  target="_blank" rel="noopener noreferrer" href="https://github.com/mhdsadique/nyka_clone">  <Button
+      w='130px'
+  p={4}
+  color='white'
+  fontWeight='bold'
+  borderRadius='md'
+   bgGradient='linear(to-l, #7928CA, #FF0080)'
+  _hover={{
+    bgGradient: 'linear(to-r, red.500, yellow.500)',
+  }}>Source Code
+  <Avatar size='xs' src='https://www.bing.com/th?id=OSK.b15146a8bebfb993c6dad4725c884d6d&w=148&h=148&c=7&o=6&dpr=1.3&pid=SANGAM' alt='github'/>
+  </Button>
+  </a>
+<a  target="_blank" rel="noopener noreferrer" href="https://secretive-form-5947-umber.vercel.app/">
+<Button
+      w='100px'
+  p={4}
+  color='white'
+  fontWeight='bold'
+  borderRadius='md'
+   bgGradient='linear(to-l, #7928CA, #FF0080)'
+  _hover={{
+    bgGradient: 'linear(to-r, red.500, yellow.500)',
+  }}>Live <ArrowRightIcon/>
+  </Button>
+  </a>
+  </Box>
+  </Grid>
+      </Box>
+      {/* ////box 2 */}
       <Box  bgGradient= 'linear(to-r, red.500, yellow.500)'  boxShadow='dark-lg' p='6' rounded='md'  color={'rgba(0,0,0,.9)'}>
   <Grid gap={"20px"}>
       <Box border="solid 1px white" w={"100%"} h="100%">
@@ -18,10 +104,10 @@ export const Project = () => {
       </Box>
 
 
-        <Heading color={'white'} marginTop={'10px'}   size={'md'}>Sephora </Heading>
-        <Text  fontSize={'2xl'} >Sephora.com is leading E-commerce web applicaton which provides wide range of beauty products.</Text>
+        <Heading color={'white'} marginTop={'10px'}   size={'md'}>Nemow </Heading>
+        <Text  fontSize={'2xl'} >Nemow.com is leading E-commerce web applicaton which provides wide range of beauty products.</Text>
         
-        <Text ><Text as='b'>Tech Stack :</Text> React.js | Redux | Chackra UI | HTML | CSS | Javascript</Text>
+        <Text ><Text as='b'>Tech Stack :</Text> React.js | Redux | Chackra UI | HTML | CSS | Javascript | Redux</Text>
         <Spacer/>
         <Box  w={'80%'} display='flex' justifyContent='space-around'  >
         <a  target="_blank" rel="noopener noreferrer" href="https://github.com/gprasadbhat948231/unequaled-cracker-7155">  <Button
@@ -112,7 +198,7 @@ Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra products.</Tex
         <Text><Text as='b'>Tech Stack :</Text>  HTML | CSS | Javascript</Text>
         <Spacer/>
         <Box w={'80%'} display='flex' justifyContent='space-around'>
-      
+       
         <a  target="_blank" rel="noopener noreferrer" href="https://github.com/Gauravkumar03/myntra-website">  <Button
       w='130px'
   p={4}
@@ -142,92 +228,9 @@ Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra products.</Tex
   </Box>
   </Grid>
       </Box>
-        {/* ---------box4 */}
-      <Box bgGradient= 'linear(to-r, red.500, yellow.500)'  boxShadow='dark-lg' p='6' rounded='md'  color={'rgba(0,0,0,.9)'}>
-        <Grid gap={'20px'}>
-      <Box>
-        <img src={nyka} alt="mailtrap" srcset="" />
-      </Box>
-
-        <Heading color={'white'} marginTop={'10px'} size={'md'}>Nyka </Heading>
-        <Text fontSize={'2xl'}>Nyka is an e-commerce website where all types of Skincare, Eyecare, Hairstyle and many more related products are available for men, women and kids.</Text>
-        
-        <Text><Text as='b'>Tech Stack :</Text> Next.js | Redux | Chackra UI | CSS | Javascript</Text>
-        <Spacer/>
-        <Box w={'80%'} display='flex' justifyContent='space-around'>
-      
-        <a  target="_blank" rel="noopener noreferrer" href="https://github.com/mhdsadique/nyka_clone">  <Button
-      w='130px'
-  p={4}
-  color='white'
-  fontWeight='bold'
-  borderRadius='md'
-   bgGradient='linear(to-l, #7928CA, #FF0080)'
-  _hover={{
-    bgGradient: 'linear(to-r, red.500, yellow.500)',
-  }}>Source Code
-  <Avatar size='xs' src='https://www.bing.com/th?id=OSK.b15146a8bebfb993c6dad4725c884d6d&w=148&h=148&c=7&o=6&dpr=1.3&pid=SANGAM' alt='github'/>
-  </Button>
-  </a>
-<a  target="_blank" rel="noopener noreferrer" href="https://secretive-form-5947-umber.vercel.app/">
-<Button
-      w='100px'
-  p={4}
-  color='white'
-  fontWeight='bold'
-  borderRadius='md'
-   bgGradient='linear(to-l, #7928CA, #FF0080)'
-  _hover={{
-    bgGradient: 'linear(to-r, red.500, yellow.500)',
-  }}>Live <ArrowRightIcon/>
-  </Button>
-  </a>
-  </Box>
-  </Grid>
-      </Box>
+   
         {/* ---------box5 */}
-      <Box   bgGradient='linear(to-r, red.500, yellow.500)'  boxShadow='dark-lg' p='6' rounded='md' color={'rgba(0,0,0,.9)'}>
-       <Grid gap={'20px'}>
-      <Box>
-        <img src={kfc} alt="kfc" srcset="" />
-      </Box>
-
-        <Heading color={'white'} marginTop={'10px'} size={'md'}>KFC </Heading>
-        <Text fontSize={'2xl'}>KFC is a popular fast food chain that serves chicken , biscuits and other delicious dishes.</Text>
-        
-        <Text ><Text as='b'>Tech Stack :</Text> React.js | Chackra UI | HTML | CSS | Javascript</Text>
-        <Spacer/>
-        <Box w={'80%'} display='flex' justifyContent='space-around'>
-      
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/mhdsadique/utopian-magic-4570-kfc">  <Button
-      w='130px'
-  p={4}
-  color='white'
-  fontWeight='bold'
-  borderRadius='md'
-   bgGradient='linear(to-l, #7928CA, #FF0080)'
-  _hover={{
-    bgGradient: 'linear(to-r, red.500, yellow.500)',
-  }}>Source Code
-  <Avatar size='xs' src='https://www.bing.com/th?id=OSK.b15146a8bebfb993c6dad4725c884d6d&w=148&h=148&c=7&o=6&dpr=1.3&pid=SANGAM' alt='github'/>
-  </Button>
-  </a>
-<a target="_blank" rel="noopener noreferrer" href="https://my-app-wheat-ten.vercel.app/">
-<Button
-      w='100px'
-  p={4}
-  color='white'
-  fontWeight='bold'
-  borderRadius='md'
-   bgGradient='linear(to-l, #7928CA, #FF0080)'
-  _hover={{
-    bgGradient: 'linear(to-r, red.500, yellow.500)',
-  }}>Live <ArrowRightIcon/>
-  </Button>
-  </a>
-  </Box>
-  </Grid>
-      </Box>
+    
       </SimpleGrid>
     </Box>
   )
